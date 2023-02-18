5f3ade387bed1cb4ff354963530693d175b57e86
SahilPatil2002 <sahilpatil1616@gmail.com>
2023-02-18 15:13:50 +0530

    changes in navbar

--- js/main.js
+++ js/main.js
@@ -11,6 +11,21 @@ function getMovies(searchText){
     axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=22255093')
         .then((response) => {
             console.log(response.data);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) =>{
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>    
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>           
+                </div>
+                `;
+            });
+
+            $('#movies').html(output);
         })
         .catch((err)=>{
             console.log(err);
